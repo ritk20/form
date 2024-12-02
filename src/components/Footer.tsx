256f23c21e150d5f82cb90e19b542f26b125da5a
ritk20 <ritobrotokalita@gmail.com>
2024-12-02 23:49:50 +0530

    fix positioning

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="text-white py-4">
+    <footer className="text-white py-4 relative bottom-0 w-full">
       <p className="text-center">&copy; {new Date().getFullYear()} Your Company. All rights reserved.</p>
     </footer>
   );
